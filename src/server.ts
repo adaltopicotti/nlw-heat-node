667ea5001c7851c29fd430a32ae992811d8b36c2
Adalto Picotti Junior <adaltopicotti.jr@gmail.com>
2021-10-22 00:47:31 -0300

    update package.json

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import "dotenv/config";
 import { serverHttp } from "./app";
 
-serverHttp.listen(process.env.PORT, () => console.log(`🔥 Server is running on PORT ${process.env.PORT}`));
+serverHttp.listen(process.env.PORT || 3000, () => console.log(`🔥 Server is running on PORT ${process.env.PORT}`));
